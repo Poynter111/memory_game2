f2863641b84193a93c0a3a14c277c67551170a36
Alex Poynter <alexpoynter@Alexs-MacBook-Pro.local>
2018-03-04 15:20:15 +0000

    Final Draft nxt patch2 days

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 //var cards = ["Queen", "Queen", "King", "King"];
-var cards = [
-   {
-       rank: "Queen",
-       suit: "hearts",
-       cardImage: "images/queen-of-hearts.png",
-   },
-   {
-      rank: "Queen",
-      suit: "diamonds",
-      cardImage: "images/queen-of-diamonds.png",
-   },
-   {
-    rank: "King",
-    suit: "hearts",
-    cardImage: "images/king-of-hearts.png",
-   },
-   {
-    rank: "King",
-    suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png",
-   },
-];
+/*
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -34,21 +13,63 @@ var flipcard = function (cardId) {
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
-if (cardsInPlay.length === 2) {
-	checkForMatch();
+createBoard(); */
+//----------------------------------------------
+console.log("Up and running!");
+var cards = [
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
+
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
+];
+var cardsInPlay = [];
+ function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+      } else {
+          alert("Sorry, try again");
+        };
+  };
+// Function to flip the cards and retreving the correct image for cards from object
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+ };
 };
+//Function to create the game board in HTML
+console.log(cardsInPlay);
+ function createBoard() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+  }
 };
 
-
-
-flipcard(2);
-flipcard(3);
-
-
-
-
-
+createBoard();
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardTwo);
 
@@ -71,3 +92,8 @@ flipcard(3);
 //        // some code
 //    }
 //}
+
+
+
+
+
